bedf4aca8dfb484e08b922736df36b6ead0e4fbb
HopeSamuel <113640033+HopeSamuel@users.noreply.github.com>
2023-08-18 20:06:51 +0100

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,59 +1,57 @@
-#!/usr/bin/python3
-"""
-Script that reads stdin line by line and computes metrics:
-- Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status
-                code> <file size>
-- After every 10 lines and/or a keyboard interruption (CTRL + C), print these
-    statistics from the beginning:
-Example:
-    File size: 5213
-    200: 2
-    401: 1
-    403: 2
-    404: 1
-    405: 1
-    500: 3
-"""
-import sys
-stcd = {"200": 0, "301": 0, "400": 0, "401": 0,
-        "403": 0, "404": 0, "405": 0, "500": 0}
-summ = 0
-
-
-def prn_stats():
-    """
-    Function that print stats about log
-    """
-    global summ
-
-    print('File size: {}'.format(summ))
-    stcdor = sorted(stcd.keys())
-    for each in stcdor:
-        if stcd[each] > 0:
-            print('{}: {}'.format(each, stcd[each]))
-
-
-if __name__ == "__main__":
-    cnt = 0
-    try:
-        """ Iter the standar input """
-        for data in sys.stdin:
-            try:
-                fact = data.split(' ')
-                """ If there is a status code """
-                if fact[-2] in stcd:
-                    stcd[fact[-2]] += 1
-                """ If there is a lenght """
-                summ += int(fact[-1])
-            except:
-                pass
-            """ Printing control """
-            cnt += 1
-            if cnt == 10:
-                prn_stats()
-                cnt = 0
-    except KeyboardInterrupt:
-        prn_stats()
-        raise
-    else:
-        prn_stats()
+#!/usr/bin/python3 
+ """ 
+ Read stdin line by line and computes metrics 
+ Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1" 
+ <status code> <file size>, skip line if not this format 
+ After every 10minutes or keyboard interrupt (CTRL + C) 
+ print these from beginning: number of lines by status code 
+ possible status codes: 200, 301, 400, 401, 404, 405, and 500 
+ if status code isn't an integer, do not print it 
+ format: <status code>: <number> 
+ Status code must be printed in ascending order 
+ """ 
+ import sys 
+  
+  
+ def print_msg(codes, file_size): 
+     print("File size: {}".format(file_size)) 
+     for key, val in sorted(codes.items()): 
+         if val != 0: 
+             print("{}: {}".format(key, val)) 
+  
+  
+ file_size = 0 
+ code = 0 
+ count_lines = 0 
+ codes = { 
+     "200": 0, 
+     "301": 0, 
+     "400": 0, 
+     "401": 0, 
+     "403": 0, 
+     "404": 0, 
+     "405": 0, 
+     "500": 0 
+ } 
+  
+ try: 
+     for line in sys.stdin: 
+         parsed_line = line.split() 
+         parsed_line = parsed_line[::-1] 
+  
+         if len(parsed_line) > 2: 
+             count_lines += 1 
+  
+             if count_lines <= 10: 
+                 file_size += int(parsed_line[0]) 
+                 code = parsed_line[1] 
+  
+                 if (code in codes.keys()): 
+                     codes[code] += 1 
+  
+             if (count_lines == 10): 
+                 print_msg(codes, file_size) 
+                 count_lines = 0 
+  
+ finally: 
+     print_msg(codes, file_size)
